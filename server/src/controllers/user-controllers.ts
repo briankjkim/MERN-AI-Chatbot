2da2b6b22fb39582053f3793ea605effe6b3686f
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-12 22:26:47 -0700

    Implement userSignUp function that takes request body data and save a new User in MongoDB

--- server/src/controllers/user-controllers.ts
+++ server/src/controllers/user-controllers.ts
@@ -1,3 +1,4 @@
+import { compare, hash } from "bcrypt";
 import User from "../models/User.js";
 import { NextFunction, Request, Response } from "express";
 
@@ -12,5 +13,48 @@ export const getAllUsers = async (
   } catch (error) {
     console.log("Error: ", error);
     return res.status(200).json({ message: "Error", cause: error.message });
+  }
+};
+
+export const userSignUp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(401).send("User already registered.");
+    const hashedPassword = await hash(password, 12);
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+    return res.status(201).json({ message: "OK", id: newUser._id.toString() });
+  } catch (error) {
+    console.log("Error: ", error);
+    return res.status(500).json({ message: "Error", cause: error.message });
+  }
+};
+
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+    const foundUser = await User.findOne({ email });
+    if (!foundUser) {
+      return res.status(401).send("User not found.");
+    }
+    const isPasswordMatched = await compare(password, foundUser.password);
+    if (!isPasswordMatched) {
+      return res.status(403).send("Incorrect Password.");
+    }
+    return res
+      .status(201)
+      .json({ message: "OK", id: foundUser._id.toString() });
+  } catch (error) {
+    console.log("Error: ", error);
+    return res.status(500).json({ message: "Error", cause: error.message });
   }
 };

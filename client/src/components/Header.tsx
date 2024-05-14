5a7494d2bada13e63c5ed8a602ab42cf67eca96e
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-14 00:39:42 -0700

    Set up scaffold code for essential components

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import React from "react";
+import Logo from "./shared/Logo";
+
+const Header = () => {
+  return (
+    <AppBar
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
+    >
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;

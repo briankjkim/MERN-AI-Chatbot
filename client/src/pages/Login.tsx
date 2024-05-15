a2ddadf38451b4023df0785a2ae49d1fbb29e2ee
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-15 15:39:10 -0700

    Implement Login button in Login page and connect with useAuth; Add Toaster to show Login-related messages

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,24 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import CustomizedInput from "../components/shared/CustomizedInput";
+import { IoIosLogIn } from "react-icons/io";
+import { useAuth } from "../context/AuthContext";
+import toast from "react-hot-toast";
 
 const Login = () => {
+  const auth = useAuth();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    try {
+      toast.loading("Signing In 😎!", { id: "login" });
+      await auth?.login(email, password);
+      toast.success("Signed In Successfully! 😍", { id: "login" });
+    } catch (error) {
+      toast.error("Error in Signing In 🥲", { id: "login" });
+    }
+  };
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
@@ -16,6 +34,7 @@ const Login = () => {
         mt={16}
       >
         <form
+          onSubmit={handleSubmit}
           style={{
             margin: "auto",
             padding: "30px",
@@ -39,6 +58,26 @@ const Login = () => {
             >
               Login
             </Typography>
+            <CustomizedInput type="email" name="email" label="Email" />
+            <CustomizedInput type="password" name="password" label="Password" />
+            <Button
+              type="submit"
+              sx={{
+                px: 2,
+                py: 1,
+                mt: 2,
+                width: 430,
+                borderRadius: 2,
+                bgcolor: "#00fffc",
+                ":hover": {
+                  bgcolor: "white",
+                  color: "black",
+                },
+              }}
+              endIcon={<IoIosLogIn />}
+            >
+              Login
+            </Button>
           </Box>
         </form>
       </Box>

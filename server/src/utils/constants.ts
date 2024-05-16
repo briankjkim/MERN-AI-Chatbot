b058597f42f5cd1f6fecfb1872fca8a7bb493951
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-15 17:34:36 -0700

    Update constants

--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -1,1 +1,6 @@
-export const COOKIE_NAME = "auth_token";
+const COOKIE_NAME = "auth_token";
+const ROLE_USER = "user";
+const MSG_USER_NOT_FOUND = "User not registered or Token is missing.";
+const GPT_MODEL = "gpt-3.5-turbo";
+const MSG_ERR_500 = "Internal Server Error.";
+export { COOKIE_NAME, MSG_USER_NOT_FOUND, ROLE_USER, GPT_MODEL, MSG_ERR_500 };

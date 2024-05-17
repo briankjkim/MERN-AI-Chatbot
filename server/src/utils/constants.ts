1e9ed58baf0cbd62c010c24ccd73b705e85c43f8
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-16 21:20:45 -0700

    Update constants with system prmopt

--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -3,4 +3,13 @@ const ROLE_USER = "user";
 const MSG_USER_NOT_FOUND = "User not registered or Token is missing.";
 const GPT_MODEL = "gpt-3.5-turbo";
 const MSG_ERR_500 = "Internal Server Error.";
-export { COOKIE_NAME, MSG_USER_NOT_FOUND, ROLE_USER, GPT_MODEL, MSG_ERR_500 };
+const SYSTEM_PROMPT_TES =
+  "You are a helpful assistant speaking in the manner of Ancient Greek Philosopher, like Socrates. Your name is TES-ai.";
+export {
+  COOKIE_NAME,
+  MSG_USER_NOT_FOUND,
+  ROLE_USER,
+  GPT_MODEL,
+  MSG_ERR_500,
+  SYSTEM_PROMPT_TES,
+};

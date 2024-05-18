11ce3f75c309a54fb55ade0141fb064cd531e5a0
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-17 19:51:39 -0700

    Create a signup page

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,7 +1,105 @@
-import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import CustomizedInput from "../components/shared/CustomizedInput";
+import { IoIosLogIn } from "react-icons/io";
+import { useAuth } from "../context/AuthContext";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    try {
+      toast.loading("Attempting to sign up a new user account 😎!", {
+        id: "signup",
+      });
+      await auth?.signup(name, email, password);
+      toast.success("Signed Up Successfully! 😍", { id: "signup" });
+      navigate("/");
+    } catch (error) {
+      toast.error("Error in Signing Up. Please contact the admin 🥲.", {
+        id: "signup",
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (auth?.user) {
+      return navigate("/chat");
+    }
+  }, [auth]);
+
+  return (
+    <Box width={"100%"} height={"100%"} display="flex" flex={1}>
+      <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
+        <img src="airobot.png" alt="Bot" style={{ width: "400px" }} />
+      </Box>
+      <Box
+        display={"flex"}
+        flex={{ xs: 1, md: 0.5 }}
+        justifyContent={"center"}
+        alignContent={"center"}
+        padding={2}
+        ml={"auto"}
+        mt={16}
+      >
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            margin: "auto",
+            padding: "30px",
+            boxShadow: "10px 10px 20px #000",
+            borderRadius: "10px",
+            border: "none",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              variant="h4"
+              textAlign="center"
+              padding={2}
+              fontWeight={600}
+            >
+              SignUp
+            </Typography>
+            <CustomizedInput type="name" name="name" label="Name" />
+            <CustomizedInput type="email" name="email" label="Email" />
+            <CustomizedInput type="password" name="password" label="Password" />
+            <Button
+              type="submit"
+              sx={{
+                px: 2,
+                py: 1,
+                mt: 2,
+                width: 430,
+                borderRadius: 2,
+                bgcolor: "#00fffc",
+                ":hover": {
+                  bgcolor: "white",
+                  color: "black",
+                },
+              }}
+              endIcon={<IoIosLogIn />}
+            >
+              SignUp
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </Box>
+  );
 };
 
 export default Signup;

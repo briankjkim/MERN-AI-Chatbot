b869cecf487ca513e2fefe8fd6c8d6faccbf4a84
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-17 20:29:06 -0700

    Update homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import TypingAnimator from "../components/typer/TypingAnimator";
 
 const Home = () => {
-  return <div>Home</div>;
+  const theme = useTheme();
+  const isBelowMd = useMediaQuery(theme.breakpoints.down("md"));
+  return (
+    <Box width={"100%"} height={"100%"} flex={"flex"} mx={"auto"}>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          flexDirection: "column",
+          alignItems: "center",
+          mx: "auto",
+        }}
+      >
+        <Box>
+          <TypingAnimator />
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: { md: "row", xs: "column", sm: "column" },
+            gap: 5,
+            my: 10,
+          }}
+        >
+          <img
+            src="robot.png"
+            alt="robot"
+            style={{ width: "200px", margin: "auto" }}
+          />
+          <img
+            className="image-inverted rotate"
+            src="openai.png"
+            alt="openai"
+            style={{ width: "200px", margin: "auto" }}
+          />
+        </Box>
+        <Box sx={{ display: "flex", width: "100%", mx: "auto" }}>
+          <img
+            src="chat.png"
+            alt="chatbot"
+            style={{
+              display: "flex",
+              margin: "auto",
+              width: isBelowMd ? "80%" : "60%",
+              borderRadius: 20,
+              boxShadow: "-5px -5px 105px #64fd5",
+              marginTop: 20,
+              marginBottom: 20,
+            }}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Home;

e411c70d3c5f105f905f53ea4d480a7ca182fbd4
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-17 19:32:08 -0700

    Protect /chat route with auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,10 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Chat from "./pages/Chat";
 import NotFound from "./pages/NotFound";
-// import { useAuth } from "./context/AuthContext";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
-  // console.log(useAuth()?.isLoggedIn);
+  const auth = useAuth();
 
   return (
     <main>
@@ -17,7 +17,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/chat" element={<Chat />} />
+        {auth?.isLoggedIn && auth.user && (
+          <Route path="/chat" element={<Chat />} />
+        )}
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </main>

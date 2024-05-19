c08059642e956bcaf77f1d67ece81f044d79ffd0
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-18 20:39:42 -0700

    Add default GET route to check server's online status

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,6 +4,10 @@ import chatRoutes from "./chat-routes.js";
 
 const appRouter = Router();
 
+appRouter.get("/", (req, res) => {
+  console.log("GET Request made at req.url: ", req.url);
+  return res.status(200).json({ message: "Server Online 🤖" });
+});
 appRouter.use("/user", userRoutes); // domain/api/v1/user
 appRouter.use("/chat", chatRoutes); // domain/api/v1/chat
 

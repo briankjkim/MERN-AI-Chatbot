730ef197618623c56405b0407c73801f635e6985
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-18 20:45:59 -0700

    update with live urls

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,12 @@ config();
 const app = express();
 
 // Middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://mern-ai-chatbot-client.vercel.app/",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
